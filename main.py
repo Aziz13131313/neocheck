6ac98e8eda84315ad4c189a3c52637cc91fd2c34
Aziz13131313 <aza.omarov90@gmail.com>
2025-06-09 16:17:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # Загрузка Excel-таблицы
 try:
-    df_stones = pd.read_excel("таблица новая123.xlsx")
+    df_stones = pd.read_excel("таблица_обновленная.xlsx")
     df_stones["Длина"] = pd.to_numeric(df_stones["Длина"], errors="coerce")
     df_stones["Ширина"] = pd.to_numeric(df_stones["Ширина"], errors="coerce")
     df_stones["Высота"] = pd.to_numeric(df_stones["Высота"], errors="coerce")
